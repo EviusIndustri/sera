b4b9bf5691bfe16a0be3f9a2c64c3df09d95573a
hdriqi <ra@evius.id>
2018-09-01 12:28:28 +0700

    update workflow

--- src/index.js
+++ src/index.js
@@ -1,51 +1,55 @@
-import express from 'express'
 import path from 'path'
 import fs from 'fs'
 
 import mime from 'mime-types'
 
-const router = express.Router()
-
 /**
  * opts {
  * spa: boolean
- * 
  * showHidden: boolean
  * }
  */
-export default (sitePath, opts) => {
+export default async (sitePath, targetPath, opts) => {
 	const options = {
 		dotfiles: opts.showHidden ? 'allow': 'deny'
 	}
 
-	router.get('*', async (req, res) => {
-		const targetPath = req.originalUrl.split('?')[0]
-		try {
-			const target = path.join(sitePath, targetPath)
-			const stat = await fs.statSync(target)
-			if(stat.isFile()) {
-				if(!mime.lookup(target)){
-					res.type('text/plain')
-				}
-				return res.sendFile(target, options)
-			}
-			else{
-				if(fs.existsSync(path.join(sitePath, targetPath, 'index.html'))) {
-					return res.sendFile(path.join(sitePath, targetPath, 'index.html'))
-				}
-				throw '404'
-			}
-		} catch(err) {
-			const targetHtml = path.join(sitePath, `${targetPath}.html`)
-			if(opts.spa == true) {
-				return res.sendFile(path.join(sitePath, `index.html`))
-			}
-			if(fs.existsSync(targetHtml)) {
-				return res.sendFile(targetHtml)
-			}
-			return res.status(404).send('404')
-		}
-	})
+	let out = {
+		status: 400,
+		path: '',
+		options: options,
+		type: null
+	}
 
-	return router
+	try {
+		const target = path.join(sitePath, targetPath)
+		const stat = await fs.statSync(target)
+		if(stat.isFile()) {
+			if(!mime.lookup(target)){
+				out.type = 'text/plain'
+			}
+			out.status = 200
+			out.path = target
+			return out
+		}
+		throw Error()
+	} catch(err) {
+		if(opts.spa == true) {
+			out.status = 200
+			out.path = path.join(sitePath, `index.html`)
+			return out
+		}
+		const targetHtml = path.join(sitePath, `${targetPath}.html`)
+		if(fs.existsSync(targetHtml)) {
+			out.status = 200
+			out.path = targetHtml
+			return out
+		}
+		if(fs.existsSync(path.join(sitePath, targetPath, 'index.html'))) {
+			out.status = 200
+			out.path = path.join(sitePath, targetPath, 'index.html')
+			return out
+		}
+		return out
+	}
 }
